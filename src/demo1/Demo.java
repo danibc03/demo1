cb11e9443025f0fa3e238cf4a12ecac012e3531a
danib <danib@Dani>
2025-02-14 00:43:18 +0100

    Esto es unaprueba

---
+++ src/demo1/Demo.java
@@ -0,0 +1,9 @@
+package demo1;
+
+public class Demo {
+	public static void main(String []args) {
+		System.out.println("Linea 1");
+		System.out.println("Linea 2");
+		System.out.println("Linea 3");
+	}
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module demo1 {
+	public
 }
